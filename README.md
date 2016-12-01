123a10095457f27aa9487ba0f7e2938f1782cab2
Nicholas Lee <nicholas.lee@ten10.com>
2016-12-01 12:05:21 +0000

    fix(standard): compiling application. updating readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TestKittUI
-TestKitt React based UI Application
+# Company Ideas Portal
+A place for people to post their ideas for how the company can improve!
---
+++ dist/1.1.03e2186e6456e17c0137.js
@@ -0,0 +1,78 @@
+webpackJsonp([1],{
+
+/***/ 597:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _reactRedux = __webpack_require__(418);
+	
+	var _IdeaOverview = __webpack_require__(598);
+	
+	var _IdeaOverview2 = _interopRequireDefault(_IdeaOverview);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var mapDispatchToProps = {};
+	
+	var mapStateToProps = function mapStateToProps(state) {
+	  return {};
+	};
+	
+	exports.default = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(_IdeaOverview2.default);
+
+/***/ },
+
+/***/ 598:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _IdeaOverview = __webpack_require__(599);
+	
+	var _IdeaOverview2 = _interopRequireDefault(_IdeaOverview);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	exports.default = _IdeaOverview2.default;
+
+/***/ },
+
+/***/ 599:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.IdeaOverview = undefined;
+	
+	var _react = __webpack_require__(24);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var IdeaOverview = exports.IdeaOverview = function IdeaOverview() {
+	  return _react2.default.createElement(
+	    'div',
+	    null,
+	    'Stuff'
+	  );
+	};
+	
+	exports.default = IdeaOverview;
+
+/***/ }
+
+});
+//# sourceMappingURL=1.1.03e2186e6456e17c0137.js.map
